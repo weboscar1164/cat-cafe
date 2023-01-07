493a46b40dd6183d968cff8d8483df02c5fe4392
weboscar1164 <cheaptrip1164@gimail.com>
2023-01-07 16:07:20 +0900

    お問い合わせ バリデーション設置

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+//お問い合わせフォーム
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('contact', [ContactController::class, 'sendMail']);
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
