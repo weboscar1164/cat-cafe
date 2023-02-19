7b106c7728aae441ae18a67a8718abc9ad733f31
weboscar1164 <cheaptrip1164@gimail.com>
2023-02-20 05:47:30 +0900

    jsバリデーション追加

--- resources/views/components/add-category-modal.blade.php
+++ resources/views/components/add-category-modal.blade.php
@@ -1,16 +1,16 @@
 <div>
-    <!-- ▼▼▼▼新しいねこ登録モーダル▼▼▼▼　-->
+    <!-- ▼▼▼▼新しいカテゴリ登録モーダル▼▼▼▼　-->
     <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="category-create-modal">
         <div class="relative w-auto my-6 mx-auto max-w-3xl">
           <!--content-->
           <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <!--header-->
-            <form action="{{ route('admin.category.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
+            <form class="validate-form" action="{{ route('admin.category.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+                <!--header-->
                 <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <h3 class="text-3xl font-semibold">
-                    カテゴリ登録
-                </h3>
+                    <h3 class="text-3xl font-semibold">
+                        カテゴリ登録
+                    </h3>
                     <button onclick="toggleModal('category-create-modal')"  type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Close modal</span>
@@ -18,38 +18,25 @@
                 </div>
                 <!--body-->
                 <div class="relative p-6 flex-auto">
-                <!-- ▼▼▼▼エラーメッセージ▼▼▼▼　-->
-                    @if($errors->any())
-                    <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                            <li class="text-red-400">{{ $error }}</li>
-                            @endforeach
-                        </ul>
+                    <div class="mb-6 validate-input">
+                        <label class="block text-sm font-medium mb-2" for="name">カテゴリ名</label>
+                        <input id="name" class="form-control validate-target block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="name" value="{{ old('name') }}" maxlength="20" required/>
+                        <div class="invalid-feedback"></div>
                     </div>
-                    @endif
-                <!-- ▲▲▲▲エラーメッセージ▲▲▲▲　-->
-                
-                <div class="mb-6">
-                     <label class="block text-sm font-medium mb-2" for="name">カテゴリ名</label>
-                     <input id="name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="name" value="{{ old('name') }}">
                 </div>
-                
-              
-            </div>
-            <!--footer-->
-            <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                <div class="px-3 mb-6">
-                    <button onclick="toggleModal('category-create-modal')" type="button" class="py-2 px-3 text-xs text-white font-semibold bg-red-500 rounded-md">閉じる</button>
-                </div>
-                <div class="px-3 mb-6">
-                    <button type="submit"class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">登録</button>
+                <!--footer-->
+                <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                    <div class="px-3 mb-6">
+                        <button onclick="toggleModal('category-create-modal')" type="button" class="py-2 px-3 text-xs text-white font-semibold bg-red-500 rounded-md">閉じる</button>
+                    </div>
+                    <div class="px-3 mb-6">
+                        <button type="submit" class="button-disabled py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md" disabled>登録</button>
+                    </div>
                 </div>
             </form>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
+    </div>
       <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="category-create-modal-backdrop">
     </div>
 </div>
